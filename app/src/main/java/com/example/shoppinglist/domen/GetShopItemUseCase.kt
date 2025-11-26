5c3127c89ef201029961f1dc0a056039fdc67964
lokko <jamm7022@gmail.com>
2025-11-26 11:14:17 +0300

    add data impl

--- app/src/main/java/com/example/shoppinglist/domen/GetShopItemUseCase.kt
+++ app/src/main/java/com/example/shoppinglist/domen/GetShopItemUseCase.kt
@@ -2,8 +2,8 @@ package com.example.shoppinglist.domen
 
 class GetShopItemUseCase(private val shopListRepository: ShopListRepository) {
 
-    fun getShopItem(shopItem: ShopItem): ShopItem{
-        return shopListRepository.getShopItem(shopItem)
+    fun getShopItem(shopItemId: Int): ShopItem{
+        return shopListRepository.getShopItem(shopItemId)
 
     }
 }
